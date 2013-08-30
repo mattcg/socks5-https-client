20644f3b49750230e865091f31be856aa0946855
Matthew Caruana Galizia <mattcg@gmail.com>
2013-08-30 16:10:11 -0600

    More portable test

--- test/index.js
+++ test/index.js
@@ -9,35 +9,77 @@
 'use strict';
 
 /*jshint node:true*/
-/*global test, suite*/
+/*global test, suite, setup*/
 
 var assert = require('assert');
+var net = require('net');
+var socks = require('node-socks/socks.js');
 var https = require('../');
 
 var version = process.version.substr(1).split('.');
 var readableStreams = version[0] > 0 || version[1] > 8;
 
 suite('socks5-https-client tests', function() {
+	var server;
+
 	this.timeout(5000);
 
+	setup(function(done) {
+		server = socks.createServer(function(socket, port, address, proxyReady) {
+			var proxy;
+
+			proxy = net.createConnection(port, address, proxyReady);
+
+			proxy.on('data', function(data) {
+				socket.write(data);
+			});
+
+			socket.on('data', function(data) {
+				proxy.write(data);
+			});
+
+			proxy.on('close', function() {
+				socket.end();
+			});
+
+			socket.on('close', function() {
+				proxy.removeAllListeners('data');
+				proxy.end();
+			});
+		});
+
+		server.listen(1080, 'localhost', 511, function() {
+			done();
+		});
+
+		server.on('error', function(err) {
+			throw err;
+		});
+	});
+
 	test('simple request', function(done) {
-		https.request({
-			socksPort: 9050, // Tor
-			port: 443,
-			hostname: 'en.wikipedia.org',
-			protocol: 'https:',
-			path: '/wiki/SOCKS'
-		}, function(res) {
+		var req;
+
+		req = https.request({
+			hostname: 'encrypted.google.com',
+			path: '/'
+		}, function(res, err) {
 			var data = '';
 
+			assert.ifError(err);
 			assert.equal(res.statusCode, 200);
+
 			res.setEncoding('utf8');
 
 			if (readableStreams) {
+
+				// The new way, using the readable stream interface (Node >= 0.10.0):
 				res.on('readable', function() {
 					data += res.read();
 				});
 			} else {
+
+				// The old way, using 'data' listeners (Node <= 0.8.22):
 				res.on('data', function(chunk) {
 					data += chunk;
 				});
@@ -49,8 +91,13 @@ suite('socks5-https-client tests', function() {
 
 				done();
 			});
-		}).on('error', function(err) {
-			assert.ifError(err);
-		}).end();
+		});
+
+		req.on('error', function(err) {
+			assert.fail(err);
+		});
+
+		// GET request, so end without sending any data.
+		req.end();
 	});
 });
