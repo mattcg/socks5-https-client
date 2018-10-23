c469eb4491f5a36b0c2e65639bc21a5777432ce8
OOP <opkampitak@hotmail.com>
2018-10-24 01:03:02 +0700

    update

--- lib/Agent.d.ts
+++ lib/Agent.d.ts
@@ -2,12 +2,11 @@ import * as https from 'https'
 declare interface AgentOptions extends https.AgentOptions{
     socksHost?: string;
     socksPort?: number;
-
 }
 declare class Agent extends https.Agent{
     constructor(options?: AgentOptions);
     options: AgentOptions;
-    socksHost?: string;
-    socksPort?: number;
+    socksHost: string;
+    socksPort: number;
 }
 export = Agent
